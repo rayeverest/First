1d46c1ba80ca4cf133f9d1a4d8385ad83c80d02b
Ray Royal <ray_royal@hotmail.com>
2025-01-20 12:36:19 -0500

    First message

---
+++ TwoTest/Test Two.py
@@ -0,0 +1,6 @@
+#write a python function to add two numbers
+def add(a,b):
+    return a+b  #return the sum of two numbers
+#provide several code options to add ten numbers
+def addTen(a,b,c,d,e,f,g,h,i,j):
+    return add(add(add(add(add(add(add(add(add(a,b),c),d),e),f),g),h),i),j) #return the sum of ten numbers
---
+++ TwoTest/Test_Copilot_Code.py
@@ -0,0 +1,19 @@
+# Python program to print the number of seconds in a year
+
+# Number of seconds in a minute
+seconds_per_minute = 60
+
+# Number of minutes in an hour
+minutes_per_hour = 60
+
+# Number of hours in a day
+hours_per_day = 24
+
+# Number of days in a year (non-leap year)
+days_per_year = 365
+
+# Calculate the number of seconds in a year
+seconds_per_year = seconds_per_minute * minutes_per_hour * hours_per_day * days_per_year
+
+# Print the result
+print(f"The number of seconds in a year is: {seconds_per_year}")
